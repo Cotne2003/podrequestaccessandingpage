7e87d6926a0476eac6a7abe7f7963db97d7c9c22
Cotne2003 <basiashvilic@gmail.com>
2023-08-18 23:46:12 +0400

    commit first

---
+++ index.js
@@ -0,0 +1,19 @@
+const emailValue = document.getElementById("emailValue");
+const button = document.getElementById("button");
+const emailError = document.getElementById("error");
+const emailPassed = document.getElementById("passed");
+const emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const emailFunct = () => {
+    if (emailValidation.test(emailValue.value)) {
+        emailError.style.display = "none"
+        emailPassed.style.display = "block"
+        emailValue.style.border = "green"
+    } else {
+        emailError.style.display = "block"
+        emailPassed.style.display = "none"
+        emailValue.style.border = "red"
+    }
+};
+
+button.addEventListener("click", emailFunct)
