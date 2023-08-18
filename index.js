b31121100b42d7367d68ad1f57b11e4f32555fc1
Cotne2003 <basiashvilic@gmail.com>
2023-08-18 23:56:48 +0400

    add code format

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const emailPassed = document.getElementById("passed");
 const emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 const emailFunct = () => {
-    if (emailValidation.test(emailValue.value)) {
-        emailError.style.display = "none"
-        emailPassed.style.display = "block"
-        emailValue.style.border = "green"
-    } else {
-        emailError.style.display = "block"
-        emailPassed.style.display = "none"
-        emailValue.style.border = "red"
-    }
+  if (emailValidation.test(emailValue.value)) {
+    emailError.style.display = "none";
+    emailPassed.style.display = "block";
+    emailValue.style.border = "green";
+  } else {
+    emailError.style.display = "block";
+    emailPassed.style.display = "none";
+    emailValue.style.border = "red";
+  }
 };
 
-button.addEventListener("click", emailFunct)
+button.addEventListener("click", emailFunct);
